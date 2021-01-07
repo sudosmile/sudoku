9d2d4cb7c5acaf5acbad20f1878d393ad87dfc59
sudosmile <mael.leclerc@epitech.eu>
2021-01-07 19:49:26 +0100

    {added}{code} done boi

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-struct coord {
+typedef struct coords {
     int x;
     int y;
     int none;
+}coord;
+
+int test_map[9][9] = {
+    {0,0,0,0,3,0,0,4,2},
+    {8,0,0,0,2,0,0,0,3},
+    {0,7,0,9,0,0,5,8,0},
+    {0,1,0,0,0,4,0,0,0},
+    {6,0,0,0,0,0,0,0,4},
+    {0,0,0,8,0,0,0,7,0},
+    {0,6,7,0,0,5,0,9,0},
+    {1,0,0,0,8,0,0,0,7},
+    {3,2,0,0,9,0,0,0,0},
 };
 
-struct coord find_empty(int map[9][9])
+coord find_empty(int map[9][9])
 {
-    struct coord out;
+    coord out;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (map[i][j] == 0) {
@@ -23,7 +35,7 @@ struct coord find_empty(int map[9][9])
     return out;
 }
 
-int valid(int map[9][9], struct coord pos, int num) //checks if move valid
+int valid(int map[9][9], coord pos, int num) //checks if move valid
 {
     int i, j, box_x, box_y;
 
@@ -55,12 +67,9 @@ int valid(int map[9][9], struct coord pos, int num) //checks if move valid
 
 int solve(int map[9][9])
 {
-    struct coord found = find_empty(map);
-    int row, col, i;
-    if (found.none != 1) {
-        row = found.x;
-        col = found.y;
-    } else
+    coord found = find_empty(map);
+    int i;
+    if (!(found.none != 1))
         return 1;
     for (i=1; i < 10; i++) {
         if (valid(map, found, i)) {
@@ -73,7 +82,32 @@ int solve(int map[9][9])
     return 0;
 }
 
+void printoku(int map[9][9])
+{
+    for (int i = 0; i < 9; i++) {
+        if (i % 3 == 0)
+            printf("--------------------\n");
+        for (int j = 0; j < 9; j++) {
+            if (j % 3 == 0)
+                printf("|");
+            printf("%i ", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void test()
+{
+    printf("original:\n");
+    printoku(test_map);
+    solve(test_map);
+    printf("solved:\n");
+    printoku(test_map);
+}
+
 int main(int arc, char *arv[])//will take user input and make a board
 {
+    test();
     return 0;
 }
